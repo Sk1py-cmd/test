8eced9b776cdcae8bb5fa1837da8e25f140a2142
Sk1py-cmd <mirzaaxmedov2001@gmail.com>
2024-07-22 23:01:48 +0500

    first commit

---
+++ console.js
@@ -0,0 +1,30 @@
+// ДЗ
+
+// const name = confirm('Твоё имя Артём ?')
+// console.log(name)
+
+// let question = prompt('Как тебя зовут ?')
+// console.log('question')
+
+// let massage = 'admin'
+// console.log('admin');
+
+// let name = 'Doming'
+// console.log(name);
+
+// console.log(`Hello $(name)`)
+// alert(`Hello ${'Артём'}`)
+
+// const a = '6'
+// const b = '3'
+
+// console.log(a / b)
+
+// Преобразования типов
+
+// const a = 100
+// const b = 0
+
+// console.log(Boolean(a))
+// console.log(Boolean(b))
+
